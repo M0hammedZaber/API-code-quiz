bc165ab06b80d59f40a9a818df37d61083d38481
Mo <mohammedzaber@hotmail.com>
2023-01-22 11:28:20 +0000

    set up and declared the DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Firstly I will create and declare the Document Object Model elements.
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
