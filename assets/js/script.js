9b7909f99632f2e53b509d6b6f8588d0a6069e25
Mo <mohammedzaber@hotmail.com>
2023-01-22 11:47:04 +0000

    Now adding the functions and making the timer do what i would like it to do along with what screen i want to be shown and not shown

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,49 @@ var feedbackEl = document.querySelector("#feedback");
 var questionIndex = 0;
 var time = questions.length * 10;
 var timing;
+
+function startQuiz() {
+    // starting with the hide start screen
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
+  
+    // using the formula to un-hide the questions section
+    questionsEl.removeAttribute("class");
+  
+    // Here I will set the timer details i want
+    clockTimer = setInterval(timing, 1000);
+
+    // This will be needed to show the starting time
+  timerEl.textContent = time;
+
+  getQuestions();
+}
+
+function getQuestions() {
+  // get current question object from array
+  var currentQuestion = questions[questionIndex];
+
+  // update title with current question
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+  // clear out any old question choices
+  choicesEl.innerHTML = "";
+
+  // loop over choices
+  
+  currentQuestion.choices.forEach(function(choice, i) {
+    // create new button for each choice
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    // attach click event listener to each choice
+    choiceNode.onclick = questionSelection;
+
+    // display on the page
+    choicesEl.appendChild(choiceNode);
+  });
+}
