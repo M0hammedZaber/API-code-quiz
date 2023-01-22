d6463a87f6998e9addbee713538334238945f081
Mo <mohammedzaber@hotmail.com>
2023-01-22 12:21:35 +0000

    added all my functions, set the checks, added click event listeners, how the quiz will have information given to the user at all stages of the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,20 +30,34 @@ function startQuiz() {
 }
 
 function getQuestions() {
-  // get current question object from array
+  // fetching the current question object from array
   var currentQuestion = questions[questionIndex];
 
-  // update title with current question
+  // A quick update of the title with the current question
   var titleEl = document.getElementById("question-title");
   titleEl.textContent = currentQuestion.title;
 
-  // clear out any old question choices
+  // the formula to remove the older choices of questions.
   choicesEl.innerHTML = "";
 
-  // loop over choices
+  // A loop will be created to go over the choices.
   
   currentQuestion.choices.forEach(function(choice, i) {
-    // create new button for each choice
+    // will make sense to create a new button for each avialable choice.
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    // An event listener will be attached to each available choice.
+    choiceNode.onclick = questionSelection;
+
+    choicesEl.appendChild(choiceNode);
+  });
+}
+currentQuestion.choices.forEach(function(choice, i) {
+    // New buttons will be created for each available choice.
     var choiceNode = document.createElement("button");
     choiceNode.setAttribute("class", "choice");
     choiceNode.setAttribute("value", choice);
@@ -53,7 +67,103 @@ function getQuestions() {
     // attach click event listener to each choice
     choiceNode.onclick = questionSelection;
 
-    // display on the page
     choicesEl.appendChild(choiceNode);
   });
+  function questionSelection() {
+    // Time will be penalized if a quessed is answered incorrectly, this will be checked here.
+    if (this.value !== questions[questionIndex].answer) {
+      // How the time will be calcualted for wrong answer and penalized.
+      time -= 10;
+  
+      if (time < 0) {
+        time = 0;
+      }
+      // New time will be shown on the page.
+      timerEl.textContent = time;
+      feedbackEl.textContent = "Oh no! That's wrong!";
+      feedbackEl.style.color = "red";
+      feedbackEl.style.fontSize = "200%";
+    } else {
+      feedbackEl.textContent = "Yay! That's correct!";
+      feedbackEl.style.color = "green";
+      feedbackEl.style.fontSize = "200%";
+    }
+  
+    // feedback will be given/flashed whether right or wrong.
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+  // Going through the questions, next question
+  questionIndex++;
+
+  // The time checker
+  if (questionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestions();
+  }
 }
+
+function quizEnd() {
+  // Need a stopper for the timer.
+  clearInterval(clockTimer);
+
+  // Need to use this to show the final end screen.
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
+
+  // Need to show the final resul/ score. 
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
+
+  // Here will be hiding the questions section.
+  questionsEl.setAttribute("class", "hide");
+}
+
+function timing() {
+  // Here the timing function will update the time.
+  time--;
+  timerEl.textContent = time;
+
+  // Does the user have enough time or has it elapsed.
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+
+function savingScores() {
+  // Need to have the data inputted within the box.
+  var initials = initialsEl.value.trim();
+
+  if (initials !== "") {
+    // Retreiving saved scores from the localstorage. Here if no scores are retrived then set to an empty array.
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // New score for current user
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+
+    // Localstorage saved here.
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    // push to another page.
+    window.location.href = "score.html";
+  }
+}
+
+function checkForEnter(event) {
+  if (event.key === "Enter") {
+    savingScores();
+  }
+}
+
+submitBtn.onclick = savingScores;
+
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
