d591fda5d40090d8bf9e4695eb8d8f8b05704525
Mo <mohammedzaber@hotmail.com>
2023-01-22 11:20:16 +0000

    added the questions and the answers to the questions for the javascripyt quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,43 @@
+var questions = [
+    {
+      title: "Is JavaScript a case-sensitive language?",
+      choices: ["True", "False"],
+      answer: "True"
+    },
+    {
+      title: "Which built-in method removes the last element from an array and returns that element?",
+      choices: [
+        "last()",
+        "get()",
+        "pop()",
+        "None of the above"
+      ],
+      answer: "pop()"
+    },
+    {
+      title: "Which of the following function of Number object forces a number to display in exponential notation?",
+      choices: ["toExponential()", "toFixed()", "toPrecision()", "toLocaleString()"],
+      answer: "toExponential()"
+    },
+    {
+      title: "Which of the following function of Boolean object returns a string containing the source of the Boolean object?",
+      choices: [
+        "toSource()",
+        "valueOf()",
+        "toString()",
+        "None of the above"
+      ],
+      answer: "toSource()"
+    },
+    {
+      title:
+        "Which of the following function of String object returns the calling string value converted to upper case?",
+      choices: [
+        "toLocaleUpperCase()",
+        "toUpperCase()",
+        "toString()",
+        "substring()"
+      ],
+      answer: "toUpperCase()"
+    },
+  ];
