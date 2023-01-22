85287bba95b9769bb6b72d3309abcc072867a1e0
Mo <mohammedzaber@hotmail.com>
2023-01-22 11:32:13 +0000

    setting up the state variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,8 @@ var submitBtn = document.querySelector("#submit");
 var startBtn = document.querySelector("#start");
 var initialsEl = document.querySelector("#initials");
 var feedbackEl = document.querySelector("#feedback");
+
+// Here I am creating the quiz state variables with a little detailed timer initiation.
+var questionIndex = 0;
+var time = questions.length * 10;
+var timing;
