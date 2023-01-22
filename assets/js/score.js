108a28e2a3ea5196e3551e224bb26635861b3a7b
Mo <mohammedzaber@hotmail.com>
2023-01-22 12:30:47 +0000

    Getting the steps for the score.js, what will be shown on the page, the highscores, how they will be sorted.

---
+++ assets/js/score.js
@@ -0,0 +1,23 @@
+function printHighscores() {
+    // Here like script.js scores either get taken from the the localstorage or set to empty array.
+    var highscores =
+    
+    JSON.parse(window.localStorage.getItem("highscores")) || [];
+ 
+    // Allignment of scores sorted in descending order.
+    highscores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+ 
+    highscores.forEach(function(score) {
+      // li's will be created for all individual high scores.
+      var liTag = document.createElement("li");
+      liTag.textContent = score.initials + " - " + score.score;
+ 
+      // what is going to be shown on page
+      var olEl = document.getElementById("highscores");
+      olEl.appendChild(liTag);
+    });
+  }
+ 
+  
